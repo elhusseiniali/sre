38d52fd4a1fd1f09e5763809d35c725eb980e8c6
Ali El Husseini <elhusseiniali@icloud.com>
2020-06-28 20:40:09 +0200

    Updated docs.

--- code/sre/test/test_products.py
+++ code/sre/test/test_products.py
@@ -9,12 +9,14 @@ class TestProduct():
     @given(lists(characters(min_codepoint=97, max_codepoint=122)))
     def test_star_atom(self, x):
         e1 = StarAtom(letters=x)
+
         p = Product([e1])
         assert p
 
     @given(characters(min_codepoint=97, max_codepoint=122))
     def test_letter_atom(self, x):
         e1 = LetterAtom(letter=x)
+
         p = Product([e1])
         assert p
 
