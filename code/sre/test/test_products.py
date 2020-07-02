ae516d6fdea053a053aa4c23fb4f0b1e98f210c7
Ph Schnoebelen <phs@lsv.fr>
2020-07-02 17:13:54 +0200

    code/sre/test: some entailment tests suggestions

--- code/sre/test/test_products.py
+++ code/sre/test/test_products.py
@@ -28,3 +28,42 @@ class TestProduct():
 
         p = Product([e1, e2])
         assert p
+
+
+
+
+
+'''Some suggestions for more entailment tests:
+Main interest is that it includes negative tests and non-trivial positive tests.
+Also it illustrates the different ways one can call a constructor.
+ea=LetterAtom('a')
+eb=LetterAtom('b')
+zs=StarAtom([])
+as=StarAtom(['a'])
+bs=StarAtom(['b'])
+abs=StarAtom(['a','b'])
+ep=Product()
+assert ea.contains(zs)
+assert ea.contains(ep)
+assert not ea.contains(as)
+assert as.contains(ea)
+assert as.contains(zs)
+assert as.contains(ep)
+assert not as.contains(bs)
+assert not as.contains(abs)
+assert not ep.contains(ea)
+assert not ep.contains(as)
+assert not ep.contains(abs)
+assert not ep.contains(Product(ea,ea))
+assert ep.contains(ep)
+assert ep.contains(zs)
+assert ep.contains(Product(zs))
+assert ep.contains(Product(zs,zs))
+assert ea.contains(zs)
+assert ea.contains(Product(zs,ep,ea,zs))
+assert not ea.contains(as)
+assert as.contains(Product(ea,as))
+assert as.contains(Product(ea,ep,zs,ea,as))
+assert abs.contains(Product(ea,bs,as,bs))
+assert not as.contains(Product(ea,abs,eb))
+'''
